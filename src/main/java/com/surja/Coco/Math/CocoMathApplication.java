759fa8b7e9dcbfbf539c6d1945adebcc1cc9abd2
vixy_mixy <palsurjendu1@gmail.com>
2023-10-07 18:52:00 +0530

    add : added security confighuration file

--- src/main/java/com/surja/Coco/Math/CocoMathApplication.java
+++ src/main/java/com/surja/Coco/Math/CocoMathApplication.java
@@ -12,14 +12,14 @@ public class CocoMathApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CocoMathApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("api/v1/*").allowedOrigins("*");
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("api/v1/*").allowedOrigins("*");
+//			}
+//		};
+//	}
+//
 }
---
+++ src/main/java/com/surja/Coco/Math/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.surja.Coco.Math.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors->cors.configurationSource(corsConfigurationSource()))
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:3000"); // Add your desired origin(s)
+        corsConfiguration.addAllowedMethod("GET"); // Add the HTTP methods you want to allow
+        corsConfiguration.addAllowedMethod("POST");
+        corsConfiguration.addAllowedMethod("PUT");
+        corsConfiguration.addAllowedMethod("DELETE");
+        corsConfiguration.addAllowedMethod("OPTIONS");
+        corsConfiguration.addAllowedHeader("*"); // Add any custom headers you want to allow
+        corsConfiguration.setAllowCredentials(true); // If you need to support cookies or authentication headers
+        corsConfiguration.setMaxAge(3600L); // The maximum time (in seconds) the CORS configuration is cached by the browser
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return  source;
+    }
+}
