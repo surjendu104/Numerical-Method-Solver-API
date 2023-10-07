771d7cd9abd4d3f694f2ebd6ce911cd5a28a513e
surjendu104 <cyber00daddy@gmail.com>
2023-10-07 20:49:45 +0530

    remove : removed security filter

--- src/main/java/com/surja/Coco/Math/config/CorsFilter.java
+++ src/main/java/com/surja/Coco/Math/config/CorsFilter.java
@@ -5,11 +5,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@WebFilter("/*")
+@Configuration
 public class CorsFilter extends OncePerRequestFilter {
 
     @Override
