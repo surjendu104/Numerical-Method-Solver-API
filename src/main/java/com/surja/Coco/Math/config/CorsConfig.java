9c0d10f884788324681f39a085fdb985b5085486
surjendu104 <cyber00daddy@gmail.com>
2023-10-08 17:19:14 +0530

    update : cors config updated - 2

--- src/main/java/com/surja/Coco/Math/config/CorsConfig.java
+++ src/main/java/com/surja/Coco/Math/config/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*");
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
